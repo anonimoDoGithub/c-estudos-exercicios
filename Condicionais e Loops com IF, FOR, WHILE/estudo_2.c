b029e71c51ba8f26be33b1f239d8b3c525f03a04
anonimoDoGithub <xxexpgamerxx@gmail.com>
2024-09-01 19:09:18 -0300

    commit 12

--- Condicionais e Loops com IF, FOR, WHILE/estudo_2.c
+++ Condicionais e Loops com IF, FOR, WHILE/estudo_2.c
@@ -2,17 +2,21 @@
 #include <math.h>
 #include <string.h>
 
+#define NUMERO_DE_CADASTRO 10 // #define é como se declara uma CONSTANTE
+
 int main(){
     printf("Cadastro de Alunos \n");
 
     char nomeDoAluno[100]; // quantidade de caracteres
 
-    for(int i = 1; i <= 5; i++){
-        printf("Cadastro %d de 5 \n", i);
+    for(int i = 1; i <= NUMERO_DE_CADASTRO; i++){
+        printf("Cadastro %d de %d \n", i, NUMERO_DE_CADASTRO);
 
         printf("Digite o nome completo do aluno: \n");
         printf("Aluno, %s\n cadastrado com sucesso! \n\n", gets(nomeDoAluno)); // gets - Leia o que o usuário digitar
 
     }
+
+    printf("Todos os alunos cadastrado, com sucesso! \n");
 
 }
